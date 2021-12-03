210749fca7f0d3453c3bded9133bc7f995c200d2
unknown <hea990@ajou.ac.kr>
2021-12-03 15:54:46 +0900

    first commit

---
+++ config/urls.py
@@ -0,0 +1,23 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('smartfarm_page.urls')),
+    # smartfarm_page/로 시작되는 페이지의 요청은 전부 smartfarm_page/urls.py에 있는 url 정보를 참고한다.
+]
---
+++ smartfarm_page/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartfarmPageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smartfarm_page'
