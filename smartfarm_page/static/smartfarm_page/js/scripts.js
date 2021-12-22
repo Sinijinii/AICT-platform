67a17e1995deb12d2bfc11b1c5004125b1794319
unknown <hea990@ajou.ac.kr>
2021-12-22 10:29:12 +0900

    211222 str_smartfarm1 edit

--- smartfarm_page/static/smartfarm_page/js/scripts.js
+++ smartfarm_page/static/smartfarm_page/js/scripts.js
@@ -5,3 +5,18 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// file upload drag & drop
+$(document).on('change', '.file-input', function() {
+
+var filesCount = $(this)[0].files.length;
+
+var textbox = $(this).prev();
+
+if (filesCount === 1) {
+var fileName = $(this).val().split('\\').pop();
+textbox.text(fileName);
+} else {
+textbox.text(filesCount + ' 개의 파일이 선택되었습니다.');
+}
+});
