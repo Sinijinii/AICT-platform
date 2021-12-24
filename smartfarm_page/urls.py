946de92d4cc475aa19e90477bfb2bd2e3204ff24
unknown <hea990@ajou.ac.kr>
2021-12-24 10:58:00 +0900

    211224 edited media file

--- smartfarm_page/urls.py
+++ smartfarm_page/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('str_smartfarm1/', views.str_smartfarm1, name='str_smartfarm1'),
     path('kids_pattern1/', views.kids_pattern1, name='kids_pattern1'),
     path('finedust1/', views.finedust1, name='finedust1'),
+    path('fileupload/', views.upload_file, name="fileupload"),
 ]
