6ef7a825adb58c73b83bceaa875760afe905dc30
Nia <yoonist08@gmail.com>
2021-12-30 10:29:00 +0900

    '211230'

--- smartfarm_page/urls.py
+++ smartfarm_page/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='main'),
     path('str_smartfarm1/', views.str_smartfarm1, name='str_smartfarm1'),
-    path('str_smartfarm2/', views.str_smartfarm2, name='str_smartfarm2'),
+    path('str_smartfarm1/str_smartfarm2/', views.str_smartfarm2, name='str_smartfarm2'),
     path('kids_pattern1/', views.kids_pattern1, name='kids_pattern1'),
     path('finedust1/', views.finedust1, name='finedust1'),
 ]
--- smartfarm_page/views.py
+++ smartfarm_page/views.py
@@ -8,14 +8,15 @@ def index(request):
 def str_smartfarm1(request):
     return render(request, 'str_smartfarm1.html')
 
+def str_smartfarm2(request):
+    return render(request, 'str_smartfarm2.html')
+
 def kids_pattern1(request):
     return render(request, 'kids_pattern1.html')
 
 def finedust1(request):
     return render(request, 'finedust1.html')
 
-def str_smartfarm2(request):
-    return render(request, 'str_smartfarm2.html')
 
 
 
