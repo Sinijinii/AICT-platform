69face33e32b5680416f45a762b01c6a73dd5223
unknown <hea990@ajou.ac.kr>
2021-12-30 17:08:54 +0900

    211230 return predicted result

--- smartfarm_page/models.py
+++ smartfarm_page/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# 앱에서 사용하는 데이터 구조를 정의하고 DB와의 소통을 담당하는 파일
+
 from django.db import models
 
 
 class FileUploadModel(models.Model):
-    file = models.FileField(null=True, upload_to="")
+    file = models.FileField(upload_to="")
+
+
+class InputValueModel(models.Model):
+    chojang = models.FloatField()
