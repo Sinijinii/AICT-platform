fe43f3f51b7955ccddd83ffd2d4d923e11578508
unknown <hea990@ajou.ac.kr>
2022-02-09 14:40:45 +0900

    temp

--- smartfarm_page/models.py
+++ smartfarm_page/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class FileUploadModel(models.Model):
     file = models.FileField(upload_to="")
 
-
 class InputValueModel(models.Model):
     chojang = models.FloatField()
 
@@ -15,8 +14,6 @@ class PhoneNumberRegex(models.Model):
     phoneNumberRegex = RegexValidator(regex=r'?([0-9]{3,4})-?([0-9]{4})$')
     phone = models.CharField(validators=[phoneNumberRegex], max_length=8, unique=False)
     # unique=False 를 통해 동일한 전화번호로 중복해서 가입 가능
-
-
 
 class User(models.Model):
     user_id = models.IntegerField(primary_key=True)
